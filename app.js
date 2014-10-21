2f529a2564375fb1c89798fb845122b6e98d8414
Tim Licata <tim@timothylicata.com>
2014-10-21 13:56:30 -0700

    add my commented out debugging code

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 var db = require("./models/index");
+
+// db.Person.create({name: "Tim"});
+// db.Person.create({name: "Elie"});
+// db.Person.create({name: "Markus"});
+// db.Person.create({name: "Ilias"});
+
+// db.Person.create({name: "James"});
+// db.Person.create({name: "Switch"});
+// db.Person.create({name: "Yoni"});
+// db.Person.create({name: "Brent"});
+
+// db.Group.create({title: "Instructors"});
+// db.Group.create({title: "Students"});
+// db.Group.create({title: "JavaScripters"});
+
+// db.Person.find({where: {name: "Tim"}}).done(function(err, peep) {
+//   db.Group.find({where: {title: "Instructors"}}).done(function(err, group) {
+//     group.addPerson(peep);
+//   });
+//   db.Group.find({where: {title: "JavaScripters"}}).done(function(err, group) {
+//     group.addPerson(peep);
+//   });
+// });
+
+// db.Person.find({where: {name: "Tim"}}).done(function(err, peep) {
+//   peep.destroy().done(function (err, success) {
+//     if (err) {
+//       console.log("couldn't delete Tim", err);
+//     } else {
+//       console.log("deleted Tim");
+//     }
+//   });
+// });
